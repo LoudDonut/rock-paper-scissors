9e59e882f9b2635e62dd1796c2f9b3eeeaf3ba7b
louddonut <roanpaulus@outlook.com>
2021-03-12 16:37:50 +0100

    Cleaned up the comments

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 function computerPlay() {
     //Calculate random number between 0 and 2 and store it
     let randNumber = Math.floor(Math.random() * 3);
-    //if number = 0 then rock etc...
     if (randNumber === 0) {
         return "rock";
     }
@@ -15,19 +14,13 @@ function computerPlay() {
     else {
         console.log("Something went wrong in computerPlay()");
     }
-    //return value
 }
-    //ASSIGN STRING TO VARIABLE
 
 function playRound(playerSelection, computerSelection) {
-    //Check the input so it is the input we want
-    //a-convert player selection to usable format
-    //Convert scissor to usable format scissors
     playerSelection = playerSelection.toLowerCase();
     if (playerSelection === 'scissor') {
         playerSelection = 'scissors';
     }
-    //b-Check input to be a string of either rock paper or scissors
     if (playerSelection === 'rock' ||
         playerSelection === 'paper' ||
         playerSelection === 'scissors') {
@@ -50,10 +43,8 @@ function playRound(playerSelection, computerSelection) {
             else {
                 return "You lost! The computer wins!";
             }
-            //Returns a string naming the winner on the console
     }
     else {
-        // If its not then ask for correct input
         console.log("Please choose either rock, paper or scissors");
     }
 }
